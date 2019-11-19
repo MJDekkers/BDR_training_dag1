f8014b88ce4252a82e9b6450dcc26d3e1ff8a30e
Anne-Marie Dekkers <anne-marie.dekkers@prorail.nl>
2019-11-19 11:16:09 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='example_pkg_mjdekkers',
     packages=find_packages(),
     version='0.1.0',
-    description='Beeldherkenning Produktiewaardig',
+    description='Beeldherkenning gereed voor produktie',
+    url='https://github.com/MJDekkers/BDR_training_dag1.git',
     author='Anne-Marie',
     license='MIT',
 )
